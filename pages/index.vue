75669fcb97d12b4def44208635ed419b288d9ed2
yaasir cheekoory <yaasir1997@gmail.com>
2024-10-18 01:45:27 +0400

    update the nav links

--- pages/index.vue
+++ pages/index.vue
@@ -57,7 +57,7 @@ const subscribeNewsletter = () => {
             <a href="/" class="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
               Home
             </a>
-            <a href="./reviews.vue" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+            <a href="/" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
               Companies
             </a>
             <NuxtLink to="./reviews" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">Reviews</NuxtLink>
@@ -66,9 +66,9 @@ const subscribeNewsletter = () => {
             </a>
           </div>
           <div class="hidden sm:ml-6 sm:flex sm:items-center">
-            <button class="bg-indigo-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <NuxtLink to="../auth/login" class="bg-indigo-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               Sign In
-            </button>
+            </NuxtLink>
           </div>
           <div class="-mr-2 flex items-center sm:hidden">
             <button @click="toggleMobileMenu" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" :aria-expanded="mobileMenuOpen">
