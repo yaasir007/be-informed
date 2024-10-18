66f0e4fdfb5d60e222b39d42192a9d74765ebcfc
yaasir cheekoory <yaasir1997@gmail.com>
2024-10-19 00:35:24 +0400

    create logo component

---
+++ pages/logo/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <NuxtLink to="./">
+    <span class="ml-2 text-xl font-bold text-black">Be</span>
+    <span class="text-xl font-bold text-[#4F45E4]">Informed</span>
+  </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
